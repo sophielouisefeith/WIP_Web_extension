7ce4a67b0b1f7fc1c471f8b701f3bff359e6235e
Kieran Hall <kieran.hall@gmail.com>
2021-11-20 17:07:10 +0100

    Stop ignoring pkg dir contents

---
+++ pkg/utils.d.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {Uint8Array}
+*/
+export function generate_secret_key(): Uint8Array;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly add_one: (a: number) => number;
+  readonly generate_secret_key: () => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/utils_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function add_one(a: number): number;
+export function generate_secret_key(): number;
+export function __wbindgen_exn_store(a: number): void;
